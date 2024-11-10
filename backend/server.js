59a1ced758d289402dea179b4dd5431ef368198c
Bhopendra Khadka <bhupenkhadka27@gmail.com>
2024-11-10 20:49:51 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/ToDoRoutes.js";
 import connectDB from "./db/conncetDB.js";
-import path from "path"; 
+
 
 dotenv.config();
-const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,12 +18,5 @@ connectDB()
 
 app.use("/api", routes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-}
-
-app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
-})
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
